bcb6d978986421d262dbcca2932c45756e28cd89
pr-a-sh-ant <111132361+pr-a-sh-ant@users.noreply.github.com>
2023-02-04 11:49:52 +0545

    First Commit

---
+++ frontend/src/app/page.js
@@ -0,0 +1,12 @@
+import RootLayout from "./layout";
+import Hero from "@/components/HomePage/hero/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="text-pink-200">PIppt</div>
+      <RootLayout></RootLayout>
+      <Hero></Hero>
+    </div>
+  );
+}
